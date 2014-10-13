fe9cdfc7553c7748c3eb2b7e50ce1dc6167b9e8d
RussellLuo <luopeng.he@gmail.com>
2014-10-13 23:02:25 +0800

    update setup.py
    1. update `install_requires`
    2. add `dependency_links`

--- setup.py
+++ setup.py
@@ -3,20 +3,32 @@
 
 from setuptools import setup
 
+description = 'A library concentrated on the Resource layer of RESTful API.'
 
-with open('README.md') as readme:
-    long_description = readme.read()
+install_requires = [
+    'jsonpatch==1.7',
+    'pymongo==2.7.2',
+    'SQLAlchemy==0.9.7',
+    'requests==2.4.1',
+    'Flask==0.10.1',
+    'mkdocs==0.9',
+]
+
+dependency_links = [
+    'git+https://github.com/RussellLuo/jsonform.git#egg=jsonform',
+    'git+https://github.com/RussellLuo/py-mongosql.git#egg=py-mongosql',
+]
 
 setup(
     name='resource',
     version='0.0.1',
     author='RussellLuo',
     author_email='luopeng.he@gmail.com',
-    description='A library concentrated on the Resource layer of RESTful API.',
+    description=description,
     license='MIT',
-    long_description=long_description,
+    long_description=description,
     packages=['resource'],
     url='https://github.com/RussellLuo/resource',
-    install_requires=['jsonform', 'jsonpatch', 'pymongo',
-                      'SQLAlchemy', 'sqlsoup', 'Flask'],
+    install_requires=install_requires,
+    dependency_links=dependency_links,
 )
