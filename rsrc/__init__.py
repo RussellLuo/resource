b894eb3aa3e6dc99565546d460549e62bc045faf
RussellLuo <luopeng.he@gmail.com>
2014-11-09 16:31:41 +0800

    support Installation from PyPI
    1. correct "setup.py" to support installing `Resource` from PyPI
    2. bump version to 0.1.0

--- rsrc/__init__.py
+++ rsrc/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = '0.1.0'
+
 from .conf import settings
 from .resource import Resource
 from .view import View
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 description = 'A library concentrated on the Resource layer of RESTful API.'
 
@@ -9,22 +9,24 @@ install_requires = [
     'jsonpatch==1.7',
     'pymongo==2.7.2',
     'SQLAlchemy==0.9.7',
+    'jsonform==0.0.1',
+    'py-mongosql==1.2.1-0',
 ]
 
 dependency_links = [
-    'git+https://github.com/RussellLuo/jsonform.git#egg=jsonform',
-    'git+https://github.com/RussellLuo/py-mongosql.git#egg=py-mongosql',
+    'https://github.com/RussellLuo/jsonform/archive/master.zip#egg=jsonform-0.0.1',
+    'https://github.com/RussellLuo/py-mongosql/archive/master.zip#egg=py-mongosql-1.2.1-0',
 ]
 
 setup(
-    name='resource',
-    version='0.0.1',
+    name='Resource',
+    version='0.1.0',
     author='RussellLuo',
     author_email='luopeng.he@gmail.com',
     description=description,
     license='MIT',
     long_description=description,
-    packages=['resource'],
+    packages=find_packages(),
     url='https://github.com/RussellLuo/resource',
     install_requires=install_requires,
     dependency_links=dependency_links,
