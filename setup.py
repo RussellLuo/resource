8c6fa334ca2f98bfa272bccc9e7c7d89df8d547c
RussellLuo <luopeng.he@gmail.com>
2014-11-21 20:18:23 +0800

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -10,17 +10,19 @@ install_requires = [
     'pymongo==2.7.2',
     'SQLAlchemy==0.9.7',
     'jsonform==0.0.1',
+    'jsonsir==0.0.1',
     'mongosql<=1.2.2',
 ]
 
 dependency_links = [
     'https://github.com/RussellLuo/jsonform/archive/master.zip#egg=jsonform-0.0.1',
+    'https://github.com/RussellLuo/jsonsir/archive/master.zip#egg=jsonsir-0.0.1',
     'https://github.com/RussellLuo/py-mongosql/archive/master.zip#egg=mongosql-1.2.2',
 ]
 
 setup(
     name='Resource',
-    version='0.1.0',
+    version='0.1.2',
     author='RussellLuo',
     author_email='luopeng.he@gmail.com',
     description=description,
