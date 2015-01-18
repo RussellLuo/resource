b76232deccb187a28ffb76272b151c49a5869102
RussellLuo <luopeng.he@gmail.com>
2015-01-18 10:58:15 +0800

    update requirements
    1. remove non-core dependent libraries from "setup.py"
    2. split "requirements.txt" into "requirements.txt" and "requirements-dev.txt"

--- setup.py
+++ setup.py
@@ -6,18 +6,9 @@ from setuptools import setup, find_packages
 description = 'A Python library concentrated on the Resource layer of RESTful APIs.'
 
 install_requires = [
-    'JsonForm==0.0.2',
-    'JsonSir==0.0.2',
-    'python-easyconfig==0.0.2',
-    'jsonpatch==1.7',
-    'pymongo==2.7.2',
-    'SQLAlchemy==0.9.7',
-    'mongosql<=1.2.2',
-    'itsdangerous==0.24',
-]
-
-dependency_links = [
-    'https://github.com/RussellLuo/py-mongosql/archive/master.zip#egg=mongosql-1.2.2',
+    'python-easyconfig>=0.1.0',
+    'JsonForm>=0.0.2',
+    'JsonSir>=0.0.2',
 ]
 
 setup(
@@ -34,5 +25,4 @@ setup(
     packages=find_packages(),
     url='https://github.com/RussellLuo/resource',
     install_requires=install_requires,
-    dependency_links=dependency_links,
 )
