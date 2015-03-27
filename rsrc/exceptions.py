a606f4fcc727e01d153cb2d1b59d2606c3fbb031
RussellLuo <luopeng.he@gmail.com>
2015-03-27 18:18:00 +0800

    add `BadRequestError` exception

--- rsrc/exceptions.py
+++ rsrc/exceptions.py
@@ -34,14 +34,9 @@ class StatusCodeError(BaseError):
         return self.detail
 
 
-class NotFoundError(StatusCodeError):
-    status_code = status.HTTP_404_NOT_FOUND
-    default_detail = 'Resource not found.'
-
-
-class MethodNotAllowedError(StatusCodeError):
-    status_code = status.HTTP_405_METHOD_NOT_ALLOWED
-    default_detail = 'Method not allowed.'
+class BadRequestError(StatusCodeError):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Bad request.'
 
 
 class UnauthorizedError(StatusCodeError):
@@ -52,3 +47,13 @@ class UnauthorizedError(StatusCodeError):
 class ForbiddenError(StatusCodeError):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = 'Forbidden.'
+
+
+class NotFoundError(StatusCodeError):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = 'Resource not found.'
+
+
+class MethodNotAllowedError(StatusCodeError):
+    status_code = status.HTTP_405_METHOD_NOT_ALLOWED
+    default_detail = 'Method not allowed.'
